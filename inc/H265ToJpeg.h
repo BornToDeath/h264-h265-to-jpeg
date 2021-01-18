74e404ec4b9eaaad7bd01369f353cc94c8ec1ee8
lxq <dearbosslee@gmail.com>
2021-01-18 12:14:13 +0800

    ffmpeg 编译成功

---
+++ inc/H265ToJpeg.h
@@ -0,0 +1,22 @@
+//
+// Created by lixiaoqing on 2021/1/13.
+//
+
+#ifndef H265TOJPEG_H265TOJPEG_H
+#define H265TOJPEG_H265TOJPEG_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * H265 帧转 Jpeg
+ * @return
+ */
+int H265ToJpeg();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif //H265TOJPEG_H265TOJPEG_H
